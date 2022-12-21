d839747278c4521d1135de2cab452eb76ccb07a7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-21 11:21:27 +0100

    Creo commenti header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -6,7 +6,16 @@ export default {
 </script>
 
 <template>
+    <!-- sezione per inserimento immagine di sfondo -->
 
+    <!-- nav bar -->
+    <!-- logo -->
+    <!-- navigazione sito -->
+
+    <!-- scritta societaria  -->
+    <!-- button -->
+
+    <!-- section in position absolute con informazioni delivery -->
 </template>
 
 <style scoped lang="scss">

80d8fcfa151c5bb4fbc99d05cf0c57d7d4d46813
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-21 11:23:45 +0100

    Conclusione commenti header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -10,12 +10,13 @@ export default {
 
     <!-- nav bar -->
     <!-- logo -->
-    <!-- navigazione sito -->
+    <!-- navigazione sito con riferimento ai data-->
 
     <!-- scritta societaria  -->
     <!-- button -->
 
     <!-- section in position absolute con informazioni delivery -->
+    <!-- section per button + e banner quadrai in position absolute -->
 </template>
 
 <style scoped lang="scss">

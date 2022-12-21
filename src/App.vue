1cb78891480e5f7143e311a95963497f55c860e7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-21 11:11:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script >
+import appHeader from './components/appHeader.vue'
+import appMain from './components/appMain.vue'
+import appFooter from './components/appFooter.vue'
+
+
+
+export default {
+  components: {
+    appHeader, appMain, appFooter
+  }
+}
+</script>
+
+<template>
+  <header>
+    <appHeader />
+  </header>
+
+  <main>
+    <appMain />
+  </main>
+
+  <footer>
+    <appFooter />
+  </footer>
+
+</template>
+
+<style lang="scss">
+@use "./components/style/general.scss"
+</style>

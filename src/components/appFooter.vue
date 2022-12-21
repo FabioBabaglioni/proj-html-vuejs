396e3b55130733717e0035771b50068b35d5582d
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-21 16:24:12 +0100

    struttura base footer

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -6,7 +6,20 @@ export default {
 </script>
 
 <template>
+    <!-- sezione per inserire il background -->
+    <section id="background">
+        <!-- sezione per rimpicciolire la width -->
+        <div class="container_big">
 
+            <!-- serie di ul con li  -->
+
+        </div>
+
+        <!-- sezione subscribe in altro componente-->
+        <div>
+
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">

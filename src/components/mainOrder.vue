d0c91a59688b937b92fd94b0d036348f393753e0
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-22 12:47:22 +0100

    Componente order completo

--- src/components/mainOrder.vue
+++ src/components/mainOrder.vue
@@ -7,12 +7,12 @@ export default {
 
 <template>
     <section id="background">
-        <div class="container_site">
-            <div class="text_medium">Dowload Our</div>
-            <div class="text_big">Ordering App</div>
+        <div class="container_site scritta">
+            <div id="text_medium">Dowload Our</div>
+            <div id="text_big">Ordering App</div>
             <div class="d_flex">
-                <img src="" alt="">
-                <img src="" alt="">
+                <a href="#"><img class="pe-4" src="../img/app-store-badge-200x67.png" alt=""></a>
+                <a href="#"><img src="../img/play-store-badge-200x67.png" alt=""></a>
             </div>
         </div>
     </section>
@@ -27,5 +27,25 @@ export default {
     background-position: top;
     height: 600px;
     margin: 100px 0;
+
+    .scritta {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        #text_medium {
+            font-size: 40px;
+            font-weight: 600;
+            color: $brandSecondary;
+            line-height: 5px;
+        }
+
+        #text_big {
+            font-size: 80px;
+            font-weight: 700;
+            color: $brandPrimary;
+        }
+    }
 }
 </style>

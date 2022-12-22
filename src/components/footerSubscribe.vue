3bcc83a5a2a973470c41263c96b4c786b3407818
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-22 10:22:54 +0100

    Aggiunta sezione subscribe footer

--- src/components/footerSubscribe.vue
+++ src/components/footerSubscribe.vue
@@ -7,9 +7,94 @@ export default {
 </script>
 
 <template>
+    <!-- immagine logo -->
+    <div id="img_logo" class="pb-4">
+        <img src="../img/takeout-logo-2x-200x43.png" alt="">
+    </div>
+
+    <!-- frase effetto -->
+    <div id="frase" class="pb-2">
+        Stay In The loop
+    </div>
+
+    <!-- frase sign up -->
+    <div id="text_medium" class="pb-4">Sign up to receive up to date news and offers directly in your inbox:</div>
+
+    <form class="pb-3">
+        <input type="text" placeholder="you@email.com" id="input" class="mb-4"> <br>
+        <button @click.prevent="" class=" btn_red btn_big">SUBSCRIBE</button>
+    </form>
+
+    <!-- social icon -->
+    <div class="d-flex justify-content-around py-4" id="social">
+        <a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" /></a>
+        <a href=""><font-awesome-icon icon="fa-brands fa-twitter" /></a>
+        <a href=""><font-awesome-icon icon="fa-brands fa-instagram" /></a>
+        <a href=""><font-awesome-icon icon="fa-brands fa-linkedin-in" /></a>
+        <a href=""><font-awesome-icon icon="fa-brands fa-youtube" /></a>
+        <a href=""><font-awesome-icon icon="fa-regular fa-envelope" /></a>
+    </div>
+
+    <!-- testo copy -->
+    <div>
+        <div class="text_small">
+            <span class="yellow"><font-awesome-icon icon="fa-solid fa-copyright" /></span>
+            <span class="yellow"> Copyright 2020 - 2022 | Avada Theme by</span>
+            <span>Fabio Babaglioni</span>
+            <span class="yellow">| All Right Reserved | Power by</span>
+            <span>Baba Srl</span>
+        </div>
+    </div>
+
 
 </template>
 
 <style scoped lang="scss">
 @use "./style/partials/variables.scss" as *;
+
+* {
+    text-align: center;
+}
+
+#frase {
+    font-size: 40px;
+    font-weight: 600;
+    color: $brandPrimary;
+}
+
+#text_medium {
+    font-size: 12px;
+}
+
+#input {
+    border-radius: 20px;
+    border: 1px solid white;
+    text-align: left;
+    padding: 5px 10px;
+    width: 40%;
+
+    &:click {
+        border: 1px solid $brandThird;
+    }
+}
+
+.btn_big {
+    width: 40%;
+    padding: 5px;
+}
+
+#social {
+    width: 20%;
+    margin: 0 auto;
+}
+
+.text_small {
+    font-size: 10px;
+    padding-bottom: 100px;
+}
+
+.yellow {
+    color: $brandPrimary;
+    background-color: rgba($color: #000000, $alpha: .0);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartShopping, faCircleRight, faClock as fasClock, faBurger, faMobileScreenButton, faCar, faPlus, faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faCircleRight, faClock as fasClock, faBurger, faMobileScreenButton, faCar, faPlus, faEnvelope as fasEnvelope, faCopyright } from '@fortawesome/free-solid-svg-icons'
 import { faClock as farClock, faEnvelope as farEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faFacebookF, faTwitter, faInstagram, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faCartShopping, faCircleRight, farClock, fasClock, faBurger, faMobileScreenButton, faCar, faPlus, fasEnvelope, farEnvelope, faTwitter, faFacebookF, faInstagram, faLinkedinIn, faYoutube)
+library.add(faCartShopping, faCircleRight, farClock, fasClock, faBurger, faMobileScreenButton, faCar, faPlus, fasEnvelope, farEnvelope, faTwitter, faFacebookF, faInstagram, faLinkedinIn, faYoutube, faCopyright)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 

9db4b19f3b9f2eb128637308765a0d3077213e95
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-22 10:13:24 +0100

    Import icone social

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartShopping, faCircleRight, faClock as fasClock, faBurger, faMobileScreenButton, faCar, faPlus } from '@fortawesome/free-solid-svg-icons'
-import { faClock as farClock } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping, faCircleRight, faClock as fasClock, faBurger, faMobileScreenButton, faCar, faPlus, faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faClock as farClock, faEnvelope as farEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faFacebookF, faTwitter, faInstagram, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faCartShopping, faCircleRight, farClock, fasClock, faBurger, faMobileScreenButton, faCar, faPlus)
+library.add(faCartShopping, faCircleRight, farClock, fasClock, faBurger, faMobileScreenButton, faCar, faPlus, fasEnvelope, farEnvelope, faTwitter, faFacebookF, faInstagram, faLinkedinIn, faYoutube)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+
+
+
+
+
 
 

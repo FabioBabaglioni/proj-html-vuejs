852962ff40f1fdc12118e5677b5caa208546a407
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-22 12:35:32 +0100

    scafolding mainOrder

--- src/components/mainOrder.vue
+++ src/components/mainOrder.vue
@@ -6,9 +6,26 @@ export default {
 </script>
 
 <template>
-
+    <section id="background">
+        <div class="container_site">
+            <div class="text_medium">Dowload Our</div>
+            <div class="text_big">Ordering App</div>
+            <div class="d_flex">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
+@use "./style/partials/variables.scss" as *;
 
+#background {
+    background-image: url(../img/app-ordering-scaled.jpg);
+    background-size: 130%;
+    background-position: top;
+    height: 600px;
+    margin: 100px 0;
+}
 </style>

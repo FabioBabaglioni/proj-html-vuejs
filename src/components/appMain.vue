ca047461cf3dd59d15d6bbc50d9afbb0ed3cc89a
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-22 10:28:01 +0100

    Creo scafolding main

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,11 +1,24 @@
 <script>
+import mainPopularDishVue from './mainPopularDish.vue';
+import mainMenuVue from './mainMenu.vue';
+import mainOrderVue from './mainOrder.vue';
+import mainNewsVue from './mainNews.vue';
 
 export default {
-
+    components: {
+        mainPopularDishVue, mainMenuVue, mainOrderVue, mainNewsVue
+    }
 }
 </script>
 
 <template>
+    <mainPopularDishVue />
+
+    <mainMenuVue />
+
+    <mainOrderVue />
+
+    <mainNewsVue />
 
 </template>
 
